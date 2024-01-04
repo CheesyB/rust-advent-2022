7d7abe37a677435d92c3666c8aafea5535fade97
Tim <nope>
2024-01-04 13:47:53 +0100

    shapes done

--- src/advent17/domain.rs
+++ src/advent17/domain.rs
@@ -1,3 +1,41 @@
+use std::ops::Add;
+use std::ops::Sub;
+
+const PLUS: Shape = Shape::new(&[
+    Point::new(1, 0),
+    Point::new(0, 1),
+    Point::new(1, 1),
+    Point::new(2, 1),
+    Point::new(1, 2),
+]);
+const MINUS: Shape = Shape::new(&[
+    Point::new(0, 0),
+    Point::new(1, 0),
+    Point::new(2, 0),
+    Point::new(3, 0),
+]);
+
+const L: Shape = Shape::new(&[
+    Point::new(0, 0),
+    Point::new(1, 0),
+    Point::new(2, 0),
+    Point::new(2, 1),
+    Point::new(2, 2),
+]);
+
+const I: Shape = Shape::new(&[
+    Point::new(0, 0),
+    Point::new(0, 1),
+    Point::new(0, 2),
+    Point::new(0, 3),
+]);
+const CUBE: Shape = Shape::new(&[
+    Point::new(0, 0),
+    Point::new(0, 1),
+    Point::new(1, 0),
+    Point::new(1, 1),
+]);
+
 pub enum Shapes {
     Plus,
     Minus,
@@ -6,17 +44,62 @@ pub enum Shapes {
     Cube,
 }
 
-pub struct Shape {
-    pub area : [char; 20]; 7],
-
-
+impl Shapes {
+    pub fn value(&self) -> &Shape {
+        match self {
+            Shapes::Plus => &PLUS,
+            Shapes::Minus => &MINUS,
+            Shapes::L => &L,
+            Shapes::I => &I,
+            Shapes::Cube => &CUBE,
+        }
+    }
 }
 
-
-
-
-pub struct Fissure {
-    pub area: [[char; 20]; 7],
+struct Point {
+    x: i32,
+    y: i32,
 }
 
+impl Point {
+    pub const fn new(x: i32, y: i32) -> Point {
+        Point { x, y }
+    }
+}
 
+impl Sub for Point {
+    type Output = Point;
+    fn sub(self, other: Point) -> <Self as Add<Point>>::Output {
+        Point {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+
+impl Add for Point {
+    type Output = Point;
+    fn add(self, other: Point) -> <Self as Add<Point>>::Output {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+struct Shape(pub &'static [Point]);
+
+// . . . .
+// . . . .
+// # # . .
+// # # . .
+//
+//
+//
+//
+
+impl Shape {
+    pub const fn new(relative_pts: &'static [Point]) -> Shape {
+        Shape(relative_pts)
+    }
+}
